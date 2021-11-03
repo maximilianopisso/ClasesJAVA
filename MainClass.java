6882c1f630953e0cee1d10ed98c8d03853ddffa3
maximiliano.pisso <maximiliano.pisso@gmail.com>
2021-11-03 10:32:28 -0300

    primer codigo

---
+++ MainClass.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MainClass{
+
+
+    public static void main(String[] args) {
+        
+
+        // Scanner input = new Scanner(System.in);
+        // Estudiante estudiante1 = new Estudiante();
+
+        Estudiante estudiante2 = new Estudiante(1, "Maximiliano Pisso", 9f, true);   //la f es por float
+
+        System.out.println(estudiante2.toString());
+
+    }
+
+
+}
